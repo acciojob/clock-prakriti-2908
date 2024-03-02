446a3eae16ebed84ef166e856810232d4fef459c
prakriti-2908 <77171197+prakriti-2908@users.noreply.github.com>
2024-03-02 23:17:57 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,42 +1,35 @@
 //your JS code here. If required.
-let para=document.querySelector("#timer")
-function timeshow()
-{
+let para = document.querySelector("#timer");
+
+function timeshow() {
     const d = new Date();
-   let month=d.getMonth();
-   let day=d.getDay();
-   let year=d.getFullYear();
+    let month = d.getMonth();
+    let day = d.getDay();
+    let year = d.getFullYear();
 
-   let hrs=d.getHours();
-   let min=d.getMinutes();
-   let sec=d.getSeconds();
-   if(min<10)
-   {
-	   min='0'+min;
-   }
-   let datetime=${month}/${day}/${year};
-   let time=converttime(hrs,min,sec)
-   
-   para.innerHTML=${datetime}, ${time}
+    let hrs = d.getHours();
+    let min = d.getMinutes();
+    let sec = d.getSeconds();
+    if (min < 10) {
+        min = '0' + min;
+    }
+    let datetime = `${month}/${day}/${year}`;
+    let time = converttime(hrs, min, sec)
+
+    para.innerHTML = `${datetime}, ${time}`;
 }
 
-function converttime(hrs,min,sec)
-{
-    if(hrs==0)
-    {
-      return 12:${min}:${sec} AM
+function converttime(hrs, min, sec) {
+    if (hrs == 0) {
+        return `12:${min}:${sec} AM`;
+    } else if (hrs > 12) {
+        return `${hrs - 12}:${min}:${sec} PM`;
+    } else if (hrs == 12) {
+        return `12:${min}:${sec} PM`;
+    } else {
+        return `${hrs}:${min}:${sec} AM`;
     }
-    else if(hrs>12)
-    {
-     return ${hrs-12}:${min}:${sec} PM
-    }
-    else if(hrs==12)
-    {
-        return ${12}:${min}:${sec} PM
-    }
-    else
-    {
-     return ${hrs}:${min}:${sec} PM
-    }
-
 }
+
+// Call the timeshow function every second
+setInterval(timeshow, 1000);
